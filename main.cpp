b07bf6ceaf0687ae1e8e7d3ae5289165e2227a67
nayyacarrisa <113011818+nayyacarrisa@users.noreply.github.com>
2022-09-07 22:43:27 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nayya Carrisa Ristradevi" << endl;
+    cout << "A11.2022.14625" << endl;
+    cout << "Jl.Kumudasmoro selatan VI No.9" << endl;
+    return 0;
+}
